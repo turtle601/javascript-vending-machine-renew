8525dd21e97ed9f2e84b78c2c17f9b9de5b66dd1
turtle601 <turtle601@naver.com>
2022-12-15 12:20:57 +0900

    feat(VendingItem) 가격이 100원 입력 에러 기능 구현

--- src/validate/VendingItemException.js
+++ src/validate/VendingItemException.js
@@ -7,8 +7,19 @@ class VendingItemException {
     this.#input = input.split(';');
   }
 
+  #isCorrectFormat() {
+    return this.#input.every((item) => REGEX.item.test(item));
+  }
+
+  #isPriceOverHundred() {
+    return this.#input.every((item) => {
+      const itemTest = item.split(',');
+      return Number(itemTest[1]) >= 100;
+    });
+  }
+
   validate() {
-    if (!this.#input.every((item) => REGEX.item.test(item))) {
+    if (!(this.#isCorrectFormat() && this.#isPriceOverHundred())) {
       throw new Error(ERROR_MESSAGE.vendingItemInput);
     }
   }
